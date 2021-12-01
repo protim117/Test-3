a8b0b65ad370de1cfdc225373187af047a2234cc
Pro Tim <protimcbpiyas@gmail.com>
2021-12-02 00:04:05 +0600

    added hover effect

--- src/components/SinglePicture/SinglePicture.js
+++ src/components/SinglePicture/SinglePicture.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './SinglePicture.css'
 
 const SinglePicture = ({picture}) => {
     const {urls,id}=picture;
     return (
         <div className='col'>
             {/* single picture after mapping  */}
-            <Link to={`picture/${id}`}><img src={urls.raw} alt="" className="w-100" /></Link> 
+            <Link to={`picture/${id}`}><img src={urls.raw} alt="" className="w-100 single-picture" /></Link> 
         </div>
     );
 };
